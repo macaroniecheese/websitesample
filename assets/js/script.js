2fffcf2a6c04baa12106254221fb45bcbe9a4280
macaroniecheese <pateljhe@gmail.com>
2025-10-28 14:11:15 -0500

    revamp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
+const logo = document.getElementById('logo');
 
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-  hamburger.classList.toggle('active');
+  const isActive = navLinks.classList.contains('show');
+
+  if (isActive) {
+    navLinks.classList.remove('show');
+    hamburger.classList.remove('active');
+    logo.src = "assets/images/logo.png";
+  } else {
+    navLinks.classList.add('show');
+    hamburger.classList.add('active');
+    logo.src = "assets/images/logo-alt.png";
+  }
 });
